59e913ba7b41982ec910d12ebb874c315ef43bf8
nachof948 <nachof948@gmail.com>
2023-12-07 21:39:23 -0300

    Cambios

--- server/index.js
+++ server/index.js
@@ -1,46 +1,47 @@
-const express = require('express')
-const app = express()
-const mostrarUsuarios = require('./routes/mostrarUsuario')
-const agregarUsuario = require('./routes/agregarUsuario')
-const eliminarUsuario = require('./routes/eliminarUsuario')
-const editarUsuario = require('./routes/editarUsuario')
-const actualizarUsuario = require('./routes/actualizarUsuario')
-const connectDB = require('./db/conexion')
-const cors = require('cors')
-
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const mostrarUsuarios = require('./routes/mostrarUsuario');
+const agregarUsuario = require('./routes/agregarUsuario');
+const eliminarUsuario = require('./routes/eliminarUsuario');
+const editarUsuario = require('./routes/editarUsuario');
+const actualizarUsuario = require('./routes/actualizarUsuario');
+const connectDB = require('./db/conexion');
+const cors = require('cors');
+require('dotenv').config();
 
 /* PUERTO */
-const PUERTO = process.env.PUERTO 
+const PUERTO = process.env.PUERTO;
+
+/* CONFIGURACIÓN DE CORS */
+app.use(cors({
+  origin: ["https://crud-mern-app-ocn8.onrender.com"],
+}));
 
 /* CONTROLAR FORMULARIO */
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/', mostrarUsuarios)
-app.use('/', eliminarUsuario)
-app.use('/', editarUsuario)
-app.use('/', actualizarUsuario)
-app.use('/agregarusuario', agregarUsuario)
+/* DEFINICIÓN DE RUTAS */
+app.use('/', mostrarUsuarios);
+app.use('/', eliminarUsuario);
+app.use('/', editarUsuario);
+app.use('/', actualizarUsuario);
+app.use('/agregarusuario', agregarUsuario);
 
-app.use(cors({
-    origin:["http://localhost:3000", "https://crud-mern-app-ocn8.onrender.com"]
-    })
-    )
+app.get('/', (req, res) => {
+  res.send('Welcome');
+});
 
-app.get('/', (req, res) =>{
-    res.send('Welcome')
-})
-/* CONEXION A LA BASE DE DATOS */
-const iniciar = async()=>{
-    try{
-        await connectDB(process.env.MONGO_URL)
-        app.listen(PUERTO,()=>{
-            console.log(`Se inicio el servidor en http://localhost:${PUERTO}`)
-        })
-    }catch(error){
-        console.log('Error al conectarse a la base de datos', error)
-    }
-}
-iniciar()
+/* CONEXIÓN A LA BASE DE DATOS */
+const iniciar = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(PUERTO, () => {
+      console.log(`Se inició el servidor en http://localhost:${PUERTO}`);
+    });
+  } catch (error) {
+    console.log('Error al conectarse a la base de datos', error);
+  }
+};
 
+iniciar();
